a622583e57753db101c57aad5fb6a05949fdf1d8
Xinyue Cui <zoey88889@gmail.com>
2025-12-23 20:41:54 -0500

    🔧 更新 login 页面与 supabase 配置

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
-window.supabase = window.supabase.createClient(
-  'https://hmbpfxjszahfibfhezft.supabase.co',
-  'sb-publishable-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYnBmeGpzemFoZmliZmhlemZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MTc3MTYsImV4cCI6MjA4MTk5MzcxNn0.t2RoqGIbRPAD2MYNVEQe6tl_dHQfN6X0NprjMqSeVqk'
-);
 
+ // 没问题，可以用！
+const supabase = window.supabase.createClient(
+ 'https://hmbpfxjszahfibfhezft.supabase.co'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYnBmeGpzemFoZmliZmhlemZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MTc3MTYsImV4cCI6MjA4MTk5MzcxNn0.t2RoqGIbRPAD2MYNVEQe6tl_dHQfN6X0NprjMqSeVqk'
+);
 // 登录状态检测 + 欢迎显示
 window.supabase.auth.getSession().then(({ data: { session } }) => {
   console.log("✅ 当前 Session：", session); // ←💥加在这里
