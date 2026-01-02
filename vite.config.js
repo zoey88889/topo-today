04cb9282cdf5a136b842133c57d31bdeaf0ae83e
Xinyue Cui <zoey88889@gmail.com>
2026-01-01 19:42:57 -0500

    Initial rebuild

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  define: {
-    'process.env': process.env // 可选，仅用于兼容旧项目
-  },
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,      // ✅ 去掉 hash
+        entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
         assetFileNames: `assets/[name].[ext]`
       }
     }
   }
 });
+
