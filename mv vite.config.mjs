b14f74cd135d823ccaa76181771c9bd577f2f8f5
Xinyue Cui <zoey88889@gmail.com>
2026-01-01 23:31:48 -0500

    ✅ 修复 Vite 配置为 ESM

---
+++ mv vite.config.mjs
@@ -0,0 +1,14 @@
+// vite.config.mjs
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
