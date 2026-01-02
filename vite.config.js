3bf13d7452f71755466535a8aa6ddb15e3b54249
Xinyue Cui <zoey88889@gmail.com>
2026-01-01 23:54:52 -0500

    ✅ 修复

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
@@ -12,4 +12,3 @@ export default defineConfig({
     }
   }
 });
-
