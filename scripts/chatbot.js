654d44fef21cd8e91b99e6d72b573c3fdb34b834
Xinyue Cui <zoey88889@gmail.com>
2026-01-04 22:33:07 -0500

    🚀billion

--- scripts/chatbot.js
+++ scripts/chatbot.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputBox = document.getElementById("inputBox");
   const sendBtn = document.getElementById("sendBtn");
   const dodobotButton = document.getElementById("dodobot-button");
-  const chatbox = document.getElementById("chatbox");
+  const chatbox = document.getElementById("dodobot-widget");
 
   sendBtn.addEventListener("click", () => {
     const userText = inputBox.value.trim();
